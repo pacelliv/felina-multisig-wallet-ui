46fff2d1fc0df5735ed8dea4ecf3327a5971c08a
PacelliV <flores.eugenio03@gmail.com>
2023-05-16 16:56:10 -0500

    new export

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,11 @@
 const transactionsDetails = require("./transactionsDetails.json")
+const transactionsDescriptions = require("./transactionsDescriptions.json")
 const tokensDetails = require("./tokensDetails.json")
 const nftsDetails = require("./nftsDetails")
 
 module.exports = {
     transactionsDetails,
+    transactionsDescriptions,
     tokensDetails,
     nftsDetails,
 }
