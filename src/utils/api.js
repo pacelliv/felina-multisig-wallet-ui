cb14ddc71d3cde9ec7baf358eb9dfdbfa358dbeb
PacelliV <flores.eugenio03@gmail.com>
2023-05-16 22:16:35 -0500

    removed global.window conditionals

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,36 +1,32 @@
 export const addTransactionDetail = async (transaction) => {
-    if (global.window) {
-        try {
-            const res = await fetch("/api/transactionsDetails", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(transaction),
-            })
-            const requestResponse = await res.json()
-            // console.log(requestResponse)
-        } catch (error) {
-            console.log(error)
-        }
+    try {
+        const res = await fetch("/api/transactionsDetails", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(transaction),
+        })
+        const requestResponse = await res.json()
+        // console.log(requestResponse)
+    } catch (error) {
+        console.log(error)
     }
 }
 
 export const updateTransactionDetail = async (id) => {
-    if (global.window) {
-        try {
-            const res = await fetch("/api/transactionsDetails", {
-                method: "PUT",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ id }),
-            })
-            const requestResponse = await res.json()
-            // console.log(requestResponse)
-        } catch (error) {
-            console.log(error)
-        }
+    try {
+        const res = await fetch("/api/transactionsDetails", {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ id }),
+        })
+        const requestResponse = await res.json()
+        // console.log(requestResponse)
+    } catch (error) {
+        console.log(error)
     }
 }
 
@@ -56,56 +52,50 @@ export const updateTransactionDetail = async (id) => {
 // }
 
 export const addNftDetail = async (nftDetail) => {
-    if (global.window) {
-        try {
-            const res = await fetch("/api/nftsDetails", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(nftDetail),
-            })
+    try {
+        const res = await fetch("/api/nftsDetails", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(nftDetail),
+        })
 
-            const requestResponse = await res.json()
-            // console.log(requestResponse)
-        } catch (error) {
-            console.log(error)
-        }
+        const requestResponse = await res.json()
+        // console.log(requestResponse)
+    } catch (error) {
+        console.log(error)
     }
 }
 
 export const deleteNftDetail = async (nftAddress, tokenId) => {
-    if (global.window) {
-        try {
-            const res = await fetch(
-                `/api/nftsDetails?nftAddress=${nftAddress}&tokenId=${tokenId}`,
-                {
-                    method: "DELETE",
-                }
-            )
-            const requestResponse = await res.json()
-            // console.log(requestResponse)
-        } catch (error) {
-            console.log(error)
-        }
+    try {
+        const res = await fetch(
+            `/api/nftsDetails?nftAddress=${nftAddress}&tokenId=${tokenId}`,
+            {
+                method: "DELETE",
+            }
+        )
+        const requestResponse = await res.json()
+        // console.log(requestResponse)
+    } catch (error) {
+        console.log(error)
     }
 }
 
 export const addTransactionDescription = async (id, description) => {
-    if (global.window) {
-        try {
-            const res = await fetch("/api/transactionsDescriptions", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ id, description }),
-            })
+    try {
+        const res = await fetch("/api/transactionsDescriptions", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ id, description }),
+        })
 
-            const requestResponse = await res.json()
-            // console.log(requestResponse)
-        } catch (error) {
-            console.log(error)
-        }
+        const requestResponse = await res.json()
+        // console.log(requestResponse)
+    } catch (error) {
+        console.log(error)
     }
 }
