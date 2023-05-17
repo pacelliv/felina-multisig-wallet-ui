462c68bb4953edcd92406bab3385bb7061a46f12
PacelliV <flores.eugenio03@gmail.com>
2023-05-17 10:35:00 -0500

    removed unused responses and unused function

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,14 +1,12 @@
 export const addTransactionDetail = async (transaction) => {
     try {
-        const res = await fetch("/api/transactions", {
+        await fetch("/api/transactions", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify(transaction),
         })
-        const requestResponse = await res.json()
-        // console.log(requestResponse)
     } catch (error) {
         console.log(error)
     }
@@ -16,53 +14,27 @@ export const addTransactionDetail = async (transaction) => {
 
 export const updateTransactionDetail = async (id) => {
     try {
-        const res = await fetch("/api/transactions", {
+        await fetch("/api/transactions", {
             method: "PUT",
             headers: {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify({ id }),
         })
-        const requestResponse = await res.json()
-        // console.log(requestResponse)
     } catch (error) {
         console.log(error)
     }
 }
 
-/* @TODO: remove this API call or find a usecase */
-// export const updateTokenBalance = async (
-//     tokenAddress,
-//     newTokenBalance,
-//     network
-// ) => {
-//     try {
-//         const res = await fetch("/api/tokens", {
-//             method: "PUT",
-//             headers: {
-//                 "Content-Type": "application/json",
-//             },
-//             body: JSON.stringify({ tokenAddress, newTokenBalance, network }),
-//         })
-//         const requestResponse = await res.json()
-//         // console.log(requestResponse)
-//     } catch (error) {
-//         console.log(error)
-//     }
-// }
-
 export const addNftDetail = async (nftDetail) => {
     try {
-        const res = await fetch("/api/nfts", {
+        await fetch("/api/nfts", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify(nftDetail),
         })
-
-        const requestResponse = await res.json()
-        // console.log(requestResponse)
     } catch (error) {
         console.log(error)
     }
@@ -70,14 +42,9 @@ export const addNftDetail = async (nftDetail) => {
 
 export const deleteNftDetail = async (nftAddress, tokenId) => {
     try {
-        const res = await fetch(
-            `/api/nfts?nftAddress=${nftAddress}&tokenId=${tokenId}`,
-            {
-                method: "DELETE",
-            }
-        )
-        const requestResponse = await res.json()
-        // console.log(requestResponse)
+        await fetch(`/api/nfts?nftAddress=${nftAddress}&tokenId=${tokenId}`, {
+            method: "DELETE",
+        })
     } catch (error) {
         console.log(error)
     }
@@ -85,16 +52,13 @@ export const deleteNftDetail = async (nftAddress, tokenId) => {
 
 export const addTransactionDescription = async (id, description) => {
     try {
-        const res = await fetch("/api/descriptions", {
+        await fetch("/api/descriptions", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify({ id, description }),
         })
-
-        const requestResponse = await res.json()
-        // console.log(requestResponse)
     } catch (error) {
         console.log(error)
     }
