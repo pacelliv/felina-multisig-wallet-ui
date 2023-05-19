b57f1769faa342d3883b503efadbe34be1b06796
PacelliV <flores.eugenio03@gmail.com>
2023-05-19 14:05:48 -0500

    set rapid api urls

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,12 +1,43 @@
+export const getPendingTransactions = async () => {
+    try {
+        const res = await fetch(
+            "https://felina-multisig-wallet-api.p.rapidapi.com/api/transactions",
+            {
+                method: "GET",
+                headers: {
+                    "X-RapidAPI-Key": process.env.NEXT_PUBLIC_X_RAPID_API_KEY,
+                    "X-RapidAPI-Host": process.env.NEXT_PUBLIC_X_RAPID_API_HOST,
+                },
+            }
+        )
+
+        const { transactionsDetails } = await res.json()
+        const filteredTransactions = transactionsDetails.filter(
+            (detail) => !detail.executed
+        )
+
+        return filteredTransactions
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 export const addTransactionDetail = async (transaction) => {
     try {
-        await fetch("/api/transactions", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(transaction),
-        })
+        await fetch(
+            "https://felina-multisig-wallet-api.p.rapidapi.com/api/transactions",
+            {
+                method: "POST",
+                headers: {
+                    "X-RapidAPI-Key": process.env.NEXT_PUBLIC_X_RAPID_API_KEY,
+                    "X-RapidAPI-Host": process.env.NEXT_PUBLIC_X_RAPID_API_HOST,
+                    "Content-Type": "application/json",
+                },
+                mode: "cors",
+                credentials: "omit",
+                body: JSON.stringify(transaction),
+            }
+        )
     } catch (error) {
         console.log(error)
     }
@@ -14,13 +45,40 @@ export const addTransactionDetail = async (transaction) => {
 
 export const updateTransactionDetail = async (id) => {
     try {
-        await fetch("/api/transactions", {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ id }),
-        })
+        await fetch(
+            "https://felina-multisig-wallet-api.p.rapidapi.com/api/transactions",
+            {
+                method: "PUT",
+                headers: {
+                    "X-RapidAPI-Key": process.env.NEXT_PUBLIC_X_RAPID_API_KEY,
+                    "X-RapidAPI-Host": process.env.NEXT_PUBLIC_X_RAPID_API_HOST,
+                    "Content-Type": "application/json",
+                },
+                mode: "cors",
+                credentials: "omit",
+                body: JSON.stringify({ id }),
+            }
+        )
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const getNfts = async () => {
+    try {
+        const res = await fetch(
+            "https://felina-multisig-wallet-api.p.rapidapi.com/api/nfts",
+            {
+                method: "GET",
+                headers: {
+                    "X-RapidAPI-Key": process.env.NEXT_PUBLIC_X_RAPID_API_KEY,
+                    "X-RapidAPI-Host": process.env.NEXT_PUBLIC_X_RAPID_API_HOST,
+                },
+            }
+        )
+        const { nfts } = await res.json()
+
+        return nfts
     } catch (error) {
         console.log(error)
     }
@@ -28,13 +86,20 @@ export const updateTransactionDetail = async (id) => {
 
 export const addNftDetail = async (nftDetail) => {
     try {
-        await fetch("/api/nfts", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(nftDetail),
-        })
+        await fetch(
+            "https://felina-multisig-wallet-api.p.rapidapi.com/api/nfts",
+            {
+                method: "POST",
+                headers: {
+                    "X-RapidAPI-Key": process.env.NEXT_PUBLIC_X_RAPID_API_KEY,
+                    "X-RapidAPI-Host": process.env.NEXT_PUBLIC_X_RAPID_API_HOST,
+                    "Content-Type": "application/json",
+                },
+                mode: "cors",
+                credentials: "omit",
+                body: JSON.stringify(nftDetail),
+            }
+        )
     } catch (error) {
         console.log(error)
     }
@@ -42,9 +107,18 @@ export const addNftDetail = async (nftDetail) => {
 
 export const deleteNftDetail = async (nftAddress, tokenId) => {
     try {
-        await fetch(`/api/nfts?nftAddress=${nftAddress}&tokenId=${tokenId}`, {
-            method: "DELETE",
-        })
+        await fetch(
+            `https://felina-multisig-wallet-api.p.rapidapi.com/?nftAddress=${nftAddress}&tokenId=${tokenId}`,
+            {
+                method: "DELETE",
+                headers: {
+                    "X-RapidAPI-Key": process.env.NEXT_PUBLIC_X_RAPID_API_KEY,
+                    "X-RapidAPI-Host": process.env.NEXT_PUBLIC_X_RAPID_API_HOST,
+                },
+                mode: "cors",
+                credentials: "omit",
+            }
+        )
     } catch (error) {
         console.log(error)
     }
@@ -52,13 +126,20 @@ export const deleteNftDetail = async (nftAddress, tokenId) => {
 
 export const addTransactionDescription = async (id, description) => {
     try {
-        await fetch("/api/descriptions", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ id, description }),
-        })
+        await fetch(
+            "https://felina-multisig-wallet-api.p.rapidapi.com/api/descriptions",
+            {
+                method: "POST",
+                headers: {
+                    "X-RapidAPI-Key": process.env.NEXT_PUBLIC_X_RAPID_API_KEY,
+                    "X-RapidAPI-Host": process.env.NEXT_PUBLIC_X_RAPID_API_HOST,
+                    "Content-Type": "application/json",
+                },
+                mode: "cors",
+                credentials: "omit",
+                body: JSON.stringify({ id, description }),
+            }
+        )
     } catch (error) {
         console.log(error)
     }
