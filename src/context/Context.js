eafb1994ce4d9e494ced97071172ea8b86ad53e4
PacelliV <flores.eugenio03@gmail.com>
2023-05-23 15:16:50 -0500

    added handleClick

--- src/context/Context.js
+++ src/context/Context.js
@@ -16,6 +16,17 @@ const ContextProvider = ({ children }) => {
     const toggleCreateTransactionModal = () =>
         setOpenCreateTransactionModal(!openCreateTransactionModal)
 
+    const handleClick = (e, id) => {
+        if (e.target.id === id) {
+            e.target.children[0].className.baseVal = "icon hidden"
+            e.target.children[1].className.baseVal = "icon show"
+            setTimeout(() => {
+                e.target.children[0].className.baseVal = "icon show"
+                e.target.children[1].className.baseVal = "icon hidden"
+            }, 1000)
+        }
+    }
+
     useEffect(() => {
         function handleResize() {
             setWindowWidth(window.innerWidth)
@@ -41,6 +52,7 @@ const ContextProvider = ({ children }) => {
                 toggleAddOwnerModal,
                 openCreateTransactionModal,
                 toggleCreateTransactionModal,
+                handleClick,
             }}
         >
             {children}
