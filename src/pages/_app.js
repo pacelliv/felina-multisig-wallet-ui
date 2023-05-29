1bb51fd2b3d55cfb151b9bb043cdaa5c6e5f20b5
PacelliV <flores.eugenio03@gmail.com>
2023-05-28 19:45:53 -0500

    removed Context.js from project

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,5 @@
 import { MoralisProvider } from "react-moralis"
 import { Web3ContextProvider } from "@/context/Web3Context"
-import { ContextProvider } from "@/context/Context"
 import Layout from "@/components/Layout"
 import "@/styles/globals.css"
 
@@ -8,11 +7,9 @@ const App = ({ Component, pageProps }) => {
     return (
         <MoralisProvider initializeOnMount={false}>
             <Web3ContextProvider>
-                <ContextProvider>
-                    <Layout>
-                        <Component {...pageProps} />
-                    </Layout>
-                </ContextProvider>
+                <Layout>
+                    <Component {...pageProps} />
+                </Layout>
             </Web3ContextProvider>
         </MoralisProvider>
     )
